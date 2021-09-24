5aff1666b1be9af48a0b92cbc503d8d2ecacb2b2
Baska <80294496+BaskaSpring@users.noreply.github.com>
2021-09-24 13:25:27 +0900

    save point 3

--- src/main/java/com/baska/UserService/UserServiceApplication.java
+++ src/main/java/com/baska/UserService/UserServiceApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-
 import java.io.IOException;
 
 
